e8f028f6ea4f5c77480aa0c93e86fc8482486b56
Andrei Borodin <andrei.borodin@dcsportsoft.com>
2025-05-09 15:50:52 +0300

    change conftest.py

--- conftest.py
+++ conftest.py
@@ -4,5 +4,6 @@ from selenium import webdriver
 @pytest.fixture()
 def driver():
     driver = webdriver.Chrome()
+    driver.get('https://qa-desk.stand.praktikum-services.ru/')
     yield driver
     driver.quit()
