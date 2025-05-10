7f10b27317ee05f826a76d016933420ce017965e
Andrei Borodin <andrei.borodin@dcsportsoft.com>
2025-05-10 10:14:11 +0300

    Change test_logout_page

--- tests/test_logout_page.py
+++ tests/test_logout_page.py
@@ -2,13 +2,15 @@ import data
 from locators import MainPageLocators, RegistrationPageLocators
 
 
-def test_check_logout_happy_path(self, ui):
-    ui.click(MainPageLocators.enter_and_registration_button)
-    ui.input_text(RegistrationPageLocators.email, data.email)
-    ui.input_text(RegistrationPageLocators.password, data.password)
-    ui.click(MainPageLocators.enter)
-    ui.click(MainPageLocators.exit)
+class TestLogoutPage:
 
-    enter_and_registration_button = ui.get_element(MainPageLocators.enter_and_registration_button)
+    def test_check_logout_happy_path(self, ui):
+        ui.click(MainPageLocators.enter_and_registration_button)
+        ui.input_text(RegistrationPageLocators.email, data.email)
+        ui.input_text(RegistrationPageLocators.password, data.password)
+        ui.click(MainPageLocators.enter)
+        ui.click(MainPageLocators.exit)
 
-    assert enter_and_registration_button.is_displayed()
+        enter_and_registration_button = ui.get_element(MainPageLocators.enter_and_registration_button)
+
+        assert enter_and_registration_button.is_displayed()
